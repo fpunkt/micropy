50b393d408ad5a4d1819c8c98e09f8a1ecfb8a2f
fpunkt <fpunkt@icloud.com>
2021-04-05 20:36:51 +0200

    Polled devices (including ping and watchdog)

---
+++ lib/sensors.py
@@ -0,0 +1,137 @@
+"""
+Misc Sensors
+
+This file uses Timer(1)
+"""
+
+# pylint: disable=import-error, missing-docstring, redefined-builtin, too-many-arguments
+
+import utime
+import machine
+import dht
+import can
+import micropython
+
+class PolledDeviceList:
+    def __init__(self):
+        self.devices = []
+        self.timer = machine.Timer(1)
+        self.stopped = False
+
+    def append(self, device):
+        self.devices.append(device)
+
+    def _next_poll(self, _):
+        if len(self.devices) == 0:
+            return
+        ticks = utime.ticks_ms()
+        for d in self.devices:
+            if utime.ticks_diff(d.next_call, ticks) <= 0:
+                d.measure()
+                d.next_call = utime.ticks_add(ticks, d.poll_intervall_in_ms)
+            else:
+                break
+        if self.stopped:
+            return
+        self.devices = sorted(self.devices, key=lambda x: x.next_call)
+
+        wait_ms = utime.ticks_diff(self.devices[0].next_call, ticks)
+        if wait_ms <= 0:
+            wait_ms = 100
+        # print('schedule next for ', wait_ms)
+        self.timer.init(period=wait_ms, mode=machine.Timer.ONE_SHOT, callback=self._irq)
+        machine.idle()
+
+    def next_poll(self):
+        self._next_poll(0)
+
+    def _irq(self, _):
+        micropython.schedule(self._next_poll, 1)
+
+    def dump(self):
+        print('{} entries'.format(len(self.devices)))
+        for d in self.devices:
+            print('  ', d)
+
+    def stop(self):
+        """Stop polling"""
+        self.stopped = True
+
+
+polled_devices = PolledDeviceList()
+
+
+class PolledDevice:
+    def __init__(self, packetid, sensorid, poll_intervall_in_ms):
+        self.packetid = packetid
+        self.sensorid = sensorid
+        if poll_intervall_in_ms < 1000:
+            poll_intervall_in_ms = 1000
+        self.poll_intervall_in_ms = poll_intervall_in_ms
+        self.next_call = utime.ticks_add(utime.ticks_ms(), poll_intervall_in_ms)
+        polled_devices.append(self)
+
+    def __repr__(self):
+        return '<{} poll interval={} ms, next in {} ms>'.format(
+            self.__class__.__name__,
+            self.poll_intervall_in_ms,
+            utime.ticks_diff(self.next_call, utime.ticks_ms()))
+
+    def send(self, payload):
+        if can.CANDevice is None:
+            return
+        bytearray = [can.CANDevice.id >> 8, can.CANDevice.id & 0xff, self.sensorid] + payload
+        can.CANDevice.send(self.packetid, bytearray)
+
+    def poll(self):
+        """poll is called in the background. The function should return max 4 bytes payload"""
+        # pylint: disable=no-self-use
+        return [1, 2]
+
+    def measure(self):
+        # print('measure', self)
+        try:
+            data = self.poll()
+        except: # pylint: disable=bare-except
+            return
+        if data is None:
+            return
+        self.send(data)
+
+class PingDevice(PolledDevice):
+    """Send ping messages"""
+    def __init__(self, poll_intervall_in_ms=300*1000):
+        super().__init__(can.CANID_PING, 0, poll_intervall_in_ms)
+
+    def send(self, _):
+        now = utime.time()
+        can.CANDevice.send(self.packetid, [
+            can.CANDevice.id >> 8, can.CANDevice.id & 0xff,
+            (now >> 24) & 0xff, (now >> 16) & 0xff, (now >> 8) & 0xff, (now >> 0) & 0xff])
+
+
+class WDT(PolledDevice):
+    """Triggers the watchdog. Does not send any message, is simply sharing
+    the timer with other polled devices"""
+    def __init__(self, poll_intervall_in_ms=4000):
+        super().__init__(0, 0, poll_intervall_in_ms)
+        self.wdt = machine.WDT(timeout=2*poll_intervall_in_ms)
+
+    def send(self, _):
+        self.wdt.feed()
+
+    def trigger(self):
+        self.wdt.feed()
+
+
+class DHT(PolledDevice):
+    """Temperature sensor"""
+    def __init__(self, pin, sensorid, poll_intervall_in_ms=60000):
+        super().__init__(can.CANID_DATALOGGER_AM2302, sensorid, poll_intervall_in_ms)
+        self.dht = dht.DHT22(pin)
+
+    def poll(self):
+        self.dht.measure()
+        t = int(10*self.dht.temperature()+0.5)
+        h = int(10*self.dht.humidity()+0.5)
+        return [h>>8, h & 0xff, t >>8, t & 0xff]
