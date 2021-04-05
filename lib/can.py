5d8d68f8942440534a1699ac678ecf7166eb5618
fpunkt <fpunkt@icloud.com>
2021-04-05 20:36:12 +0200

    Catch send errors and added config commands

--- lib/can.py
+++ lib/can.py
@@ -4,18 +4,27 @@ CAN bus interface
 
 # pylint: disable=import-error, missing-docstring, redefined-builtin, too-many-arguments
 
-import time
+import utime
 import machine
 import micropython
+import net
 
 if 0 == 1:
     # make pylint think that it knows about 'const' variable
     # pylint: disable=used-before-assignment, undefined-variable, self-assigning-variable
     const = const
-    time.sleep_ms = time.sleep_ms
+
+# CANID_POWER_ON message sent to the CAN Bus
+CANID_POWER_ON = const(0x3c4)
+CANID_DATALOGGER_AM2302 = const(0x6f1)
+CANID_PING = const(0x7e0)
+CANID_WEBREPL_STARTED = const(0x3c6)
 
 
-POWER_ON = const(0x3c4)
+# CAN command to start up WEBREPL
+START_WEBREPL = const(0xfe)
+SOFT_RESET = const(0xfd)
+HARD_RESET = const(0xfc)
 
 
 # default connects to tx=4, rx=2; BAD, because 2 == LED
@@ -24,6 +33,9 @@ POWER_ON = const(0x3c4)
 # We can however attach an IRQ handler to the RX line.
 # One an interrupt is triggered we remove the handler and re-activate it like 10ms later
 
+def _payloadstring(payload):
+    return ' '.join('{:02x}'.format(x) for x in payload)
+
 class Message:
     """A CAN message"""
     # pylint: disable=too-few-public-methods
@@ -31,8 +43,16 @@ class Message:
         self.id = id
         self.payload = payload
 
+    def payloadstring(self):
+        return _payloadstring(self.payload)
+
+    def __repr__(self):
+        return '<Message #{:03x} [{}] {}>'.format(self.id, len(self.payload), self.payloadstring())
+
+CANDevice = None
+
 class CAN:
-    """Wrapper for machine.CAN that provides some kind of interrupt and callback"""
+    """Wrapper for machine.CAN, providing (some kind of) interrupt and callback"""
     def __init__(self, id=None, rx=13, tx=12, baudrate=125, mode=machine.CAN.NORMAL):
         # bus = CAN(0, mode=CAN.NORMAL, baudrate=125, rx_io=13, tx_io=12)
         self.can = machine.CAN(0, mode=mode, baudrate=baudrate, rx_io=rx, tx_io=tx)
@@ -43,13 +63,18 @@ class CAN:
         self._subscribed_to = 0
         self.id = id
         if id is not None:
+            serial = machine.unique_id()
             self.can.send([self.id >>8, self.id & 0xff,
-            0, # startup reason
-            2, # HClib Version
-            12, # HW Type -- make this 12 for ESP32 ..
-            0xa0, # Application type and Version - make this the library version
-            0, 0], # CPU serial
-            POWER_ON)
+                    machine.reset_cause(), # startup reason
+                    2, # HClib Version
+                    12, # HW Type -- make this 12 for ESP32 ..
+                    0xa0, # Application type and Version - make this the library version
+                    serial[-2], serial[-1]], # CPU serial
+                CANID_POWER_ON)
+        self._enable_irq() # handle configuration commands
+        # pylint: disable=global-statement
+        global CANDevice
+        CANDevice = self
 
     def subscribe(self, id, callback):
         self._subscribed_to = id
@@ -64,13 +89,18 @@ class CAN:
 
     def send(self, canid, data):
         """Send data"""
-        self.can.send(data, canid)
+        for _ in range(5):
+            try:
+                self.can.send(data, canid)
+                return
+            except: # pylint: disable=bare-except
+                # print('ERROR CAN: cannot send #', i)
+                utime.sleep_ms(1)
+        print('ERROR: CAN cannot send message #{:03x} {}'.format(id, _payloadstring(data)))
 
     def _dispatch_input(self, _):
         """Called when IRQ has detected new data. This function is running outside IRQ context"""
         #print("Hier ist dispatch")
-        if self._callback is None:
-            return
         for _ in range(5):
             if self.can.any():
                 # found data
@@ -79,8 +109,11 @@ class CAN:
                     try:
                         packet = self.can.recv()
                         id = packet[0]
-                        if id == self._subscribed_to:
-                            payload = packet[3]
+                        if id != self._subscribed_to:
+                            continue
+                        payload = packet[3]
+                        if self._handle_config_command(payload):
+                            continue
                         self._callback(Message(id, payload))
                     except: # pylint: disable=bare-except
                         pass
@@ -92,8 +125,9 @@ class CAN:
                     except: # pylint: disable=bare-except
                         pass
                 return
-            time.sleep_ms(1)
+            utime.sleep_ms(1)
         self._enable_irq() # trust nobody
+        machine.idle()
 
     def _enable_irq(self):
         if self._callback is None:
@@ -111,3 +145,21 @@ class CAN:
         except: # pylint: disable=bare-except
             # OK to ignore errors here, _dispatch input will process all items in the queue
             pass
+
+    def _handle_config_command(self, payload):
+        if len(payload) == 1 and payload[0] == START_WEBREPL:
+            ip = net.connect_to_wlan()
+            print('GOT IP: ', ip)
+            net.start_repl()
+            ipx = ip[0].split('.')
+            print('GOT IPx: ', ipx)
+            self.send(CANID_WEBREPL_STARTED, [self.id >>8, self.id & 0xff, ipx[0], ipx[1], ipx[2], ipx[3]])
+            return True
+        if len(payload) == 3:
+            if payload[1] != 0xaf or payload[2] != 0xfe:
+                return False
+            if payload[0] == SOFT_RESET:
+                machine.soft_reset()
+            if payload[0] == HARD_RESET:
+                machine.reset()
+        return False
