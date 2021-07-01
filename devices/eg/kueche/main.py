3019409b8bdba3d4ef422e1655b566c2f6a93e62
fpunkt <fpunkt@icloud.com>
2021-07-01 18:25:37 +0200

    Fixed PWM for Brotdose

--- devices/eg/kueche/main.py
+++ devices/eg/kueche/main.py
@@ -1,7 +1,10 @@
 """
 Kueche main.py
 
-This file is loaded after boot.py
+To update run
+
+../../../webrepl/webrepl_cli.py -p x main.py 192.168.178.156:
+
 
 3 buttons
 8 PWM
@@ -45,19 +48,19 @@ p0.lastintensity = _defi1
 p1 = pwm.PWM(1, 4) # Fenster
 p1.lastintensity = _defi1
 
-p2 = pwm.PWM(2, 16) # Arbeitsplatte warm
+p2 = pwm.PWM(2, 16) # Dunsthaube kalt
 p2.lastintensity = _defi1
 
-p3 = pwm.PWM(3, 17) # Arbeitsplatte kalt
+p3 = pwm.PWM(3, 17) # Arbeitsplatte warm
 p3.lastintensity = _defi2
 
-p4 = pwm.PWM(4, 5) # Dunstabzug kalt
+p4 = pwm.PWM(4, 5) # Arbeitsplatte kalt
 p4.lastintensity = _defi2
 
-p5 = pwm.PWM(5, 18) # Brotdose
-p5.lastintensity = _defi1
+p5 = pwm.PWM(5, 18) # NC
 
-p6 = pwm.PWM(6, 19) # NC
+p6 = pwm.PWM(6, 19) # # Brotdose
+p6.lastintensity = _defi1
 
 p7 = pwm.PWM(7, 21) # Spüle
 p7.lastintensity = _defi1
@@ -85,8 +88,8 @@ b1.callback = cb
 b2.callback = cb
 
 pl1 = pwm.List(0x20, p0, p1, p4)
-pl2 = pwm.List(0x21, p0, p1, p2, p3, p4, p5)
-pl3 = pwm.List(0x22, p0, p1, p2, p3, p4, p5, p7)
+pl2 = pwm.List(0x21, p0, p1, p2, p3, p4, p6)
+pl3 = pwm.List(0x22, p0, p1, p2, p3, p4, p6, p7)
 # pl1.toggle_mode = 1
 
 b1.pwm = pl1
