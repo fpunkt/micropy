d75389b41fb4df8121c7970b1ac644c84198e1ba
fpunkt <fpunkt@icloud.com>
2021-09-26 12:40:11 +0200

    Moved sensor to motor and use async for control

--- devices/eg/rollladen/main.py
+++ devices/eg/rollladen/main.py
@@ -32,8 +32,8 @@ import irqio
 
 power = relais.Relais(None, bconf.ML10_PWM_8)
 
-m1 = motor.Motor(10, bconf.ML10_PWM_3, bconf.ML10_PWM_4)
-m2 = motor.Motor(12, bconf.ML10_PWM_1, bconf.ML10_PWM_2)
+m1 = motor.Motor(10, bconf.ML10_PWM_3, bconf.ML10_PWM_4, bconf.AUX3_YELLOW)
+# m2 = motor.Motor(12, bconf.ML10_PWM_1, bconf.ML10_PWM_2)
 
 m = m1
 
@@ -63,7 +63,7 @@ def can_callback(msg):
             power.on()
             return
         if command == 2:
-            m1.speed(0)
+            m1.fullstop()
             return
         if command == 3:
             m1.speed(19999)
@@ -79,34 +79,17 @@ def can_callback(msg):
 
 can.subscribe(can_callback)
 
-class Hall(irqio.IRQIO):
-    def __init__(self, sensorid, pinid):
-        super().__init__(sensorid, pinid, trigger=None, pullup=True)
-        self.count = 0
-        self.lasttrigger = 0
-
-    def run(self):
-        changed = super().run()
-        if changed:
-            now = utime.ticks_ms()
-            self.count += 1
-            if 1 == 2:
-                print('HALL #{} changed {:4d}  {:5d} ms'.format(
-                    self.sensorid, self.count, utime.ticks_diff(now, self.lasttrigger)))
-            self.lasttrigger = now
-
-
-hall1 = Hall(30, bconf.AUX3_WHITE)
-hall2 = Hall(31, bconf.AUX3_YELLOW)
 
 async def phall():
-    c1, c2 = hall1.count, hall2.count
+    c = m1.sensorcount
     mssleep = 1000
+    loopcounter = 0
     while True:
-        print('Hall {:5d} {:5.3f} /s      {:5d} {:5.3f} /s'.format(
-            hall1.count, (hall1.count - c1) *1000 / mssleep,
-            hall2.count, (hall2.count - c2) *1000 / mssleep))
-        c1, c2 = hall1.count, hall2.count
+        loopcounter += 1
+        print('Hall {:6d} {:5d} {:4d} /s'.format(
+            loopcounter,
+            m1.sensorcount, (m1.sensorcount - c) *1000 // mssleep))
+        c = m1.sensorcount
         await asyncio.sleep_ms(mssleep)
 
 board.BACKGROUND_RUNNERS.append(phall())
