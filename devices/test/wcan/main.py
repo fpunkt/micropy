9b36bbf3a2a03f5f608ffab9dcc721fcc762770b
Gogs <gogs@fake.local>
2021-11-06 22:12:19 +0100

    Snapshot

--- devices/test/wcan/main.py
+++ devices/test/wcan/main.py
@@ -20,12 +20,43 @@ import usocket as socket
 
 addr_info = socket.getaddrinfo("192.168.178.4", 14711)
 addr = addr_info[0][-1]
-print('connecting to ', addr)
+print('connecting to {}.{}.{}.{}'.format(addr[4], addr[5], addr[6], addr[7]))
 s = socket.socket()
 s.connect(addr)
 
-while True:
-    s.write(b'test')
-    time.sleep(1)
-    # data = s.recv(500)
-    # print(str(data, 'utf8'), end='')
+async def receiver():
+    sreader = asyncio.StreamReader(s)
+    while True:
+        try:
+            res = await sreader.read(12)
+            canid = (res[0]<<16) | (res[1]<<8)|res[3]
+            print('Got message ', canid)
+        except:
+            sys.exit(1)
+
+sendbuffer = bytearray([0]*12)
+
+def send(canid, buffer):
+    sendbuffer[0] = (canid >> 16) & 0xff
+    sendbuffer[1] = (canid >>  8) & 0xff
+    sendbuffer[2] = (canid >>  0) & 0xff
+    sendbuffer[3] = len(buffer)
+    sendbuffer[4:4+len(buffer)] = buffer
+    print('sending msg 0x{:03x}, len={}, bytes={}'.format(canid, len(buffer), buffer))
+    s.write(sendbuffer)
+
+async def sender():
+    while True:
+        b = bytearray([1, 2, 3])
+        send(0x123, b)
+        await asyncio.sleep(5)
+
+async def main():
+    p1 = asyncio.create_task(receiver())
+    p2 = asyncio.create_task(sender())
+    while True:
+        await asyncio.sleep(100)
+    #asyncio.gather(p1, p2)
+    #asyncio.gather(receiver(), sender())
+
+asyncio.run(main())
