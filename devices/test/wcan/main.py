edc346090c147b57428605cf3bf693649407e1dd
Gogs <gogs@fake.local>
2021-11-06 01:24:57 +0100

    Snapshot

--- devices/test/wcan/main.py
+++ devices/test/wcan/main.py
@@ -2,16 +2,30 @@
 Test the WCAN package
 """
 
+import time
+import sys
+
 try:
     import uasyncio as asyncio
 except:
     import asyncio
 
-import sys
 if sys.platform != 'linux':
     import net
     net.start_wlan()
     net.start_repl() # need for copy
 
+import usocket as socket
 
 
+addr_info = socket.getaddrinfo("192.168.178.4", 14711)
+addr = addr_info[0][-1]
+print('connecting to ', addr)
+s = socket.socket()
+s.connect(addr)
+
+while True:
+    s.write(b'test')
+    time.sleep(1)
+    # data = s.recv(500)
+    # print(str(data, 'utf8'), end='')
