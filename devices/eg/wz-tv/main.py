e418adbe9f0a213654324b40d7d74423067214a1
fpunkt <fpunkt@icloud.com>
2022-04-03 22:30:11 +0200

    Initial commit

---
+++ devices/eg/wz-tv/main.py
@@ -0,0 +1,114 @@
+"""
+WZ close to TV
+
+handles
+
+ML10 (PWM )
+- 3 x spots in roof (dimmable, change color temp with intensity)
+- 2 x spares in front of the window (xmas stars)
+- 1 x TV lamp
+- 2 x for really warm 24V LEDs
+
+RJ12
+- 2 x button - main button close to the door
+
+Groove
+- 1 x am2320
+- 1 x motion
+
+Groove
+- drive 24V DC/DC for upper two PWM channels
+
+"""
+
+#import time; print('Loading main, giving time to abort ....'); time.sleep(2)
+
+import board
+board.LOCATION = 'wztv'
+board.DEBUG = True
+board.CANID = 0x120
+
+if board.DEBUG is True:
+    # board.CANID = 0x10
+    print("This is {}, CANID {:03x}".format(board.LOCATION, 0 if board.CANID is None else board.CANID))
+
+if board.DEBUG is True:
+    import net
+    net.DEBUG = True
+    net.start_wlan(32)
+    net.start_repl()
+
+import gc
+import bconf
+import can
+import machine
+import sensors
+import pwm
+import button
+import uasyncio as asyncio
+import motionsensor
+
+if board.CAN and board.DEBUG:
+    board.CAN.cancommon.send_wlan_connected()
+
+p0 = pwm.PWM(1, bconf.ML10_PWM_1)
+p1 = pwm.PWM(2, bconf.ML10_PWM_2)
+p2 = pwm.PWM(3, bconf.ML10_PWM_3)
+p3 = pwm.PWM(4, bconf.ML10_PWM_4)
+p4 = pwm.PWM(5, bconf.ML10_PWM_5)
+p5 = pwm.PWM(6, bconf.ML10_PWM_6)
+p6 = pwm.PWM(7, bconf.ML10_PWM_7)
+p7 = pwm.PWM(8, bconf.ML10_PWM_8)
+
+# PINs on left side (buttons, thermometer and motionsensors)
+# 13, 12, 14, 27, 26, 25, 33
+b1 = button.Button(0x20, bconf.RJ12_1_WHITE_INPUT_ONLY)
+b2 = button.Button(0x21, bconf.RJ12_1_GREEN_INPUT_ONLY)
+
+def _motion_callback(x):
+    if board.DEBUG:
+        print('Motion detected on {}'.format(x))
+
+m1 = motionsensor.Motionsensor(0x30, bconf.AUX1_WHITE)
+m1.callback = _motion_callback
+
+
+def cb(but):
+    board.PRINT('got event from button {}', but)
+
+b1.callback = cb
+b2.callback = cb
+
+# pl1.toggle_mode = 1
+
+
+#
+# temperature = sensors.DHT(0x30, 33, poll_intervall_in_ms=5*60*1000)
+temperature = sensors.DHT(0x40, bconf.AUX1_YELLOW, poll_intervall_in_ms=1*30*1000)
+
+message_counter = 0
+
+
+def can_callback(msg):
+    # pylint: disable=global-statement
+    global message_counter
+    message_counter += 1
+    if len(msg.payload) > 3 and msg.payload[0] == 0x11:
+        count = 100*(msg.payload[1]<<8 + msg.payload[2])
+        print("DOING SOME STUPID LOOPING", count)
+        while count > 0:
+            count -= 1
+        print("DONE with stupid looping")
+        return
+    msg.unknown_command()
+
+
+can.subscribe(can_callback)
+
+def r():
+    board.restart()
+
+if 1 == 1: # pylint: disable=comparison-with-itself
+    board.run()
+else:
+    print('# run board.run() to start event handler')
---
+++ lib/config_rj12.py
@@ -0,0 +1,60 @@
+"""
+ESP32 board from EasyEDA
+2 x ML10 connector
+2 x RJ12 connector (6 pin)
+2 x Grove connector
+"""
+
+# pylint: disable=import-error, missing-docstring, redefined-builtin, too-many-arguments
+
+if 0 == 1:
+    # make pylint think that it knows about 'const' variable
+    const = lambda x: x
+
+import board
+
+# The on-chip LED
+board.LED = board.Led(2)
+
+RJ12_1_WHITE_INPUT_ONLY = const(39)
+RJ12_1_GREEN_INPUT_ONLY = const(36)
+RJ12_1_YELLOW = const(23)
+RJ12_1_BLUE = const(22)
+
+RJ12_2_WHITE = const(27)
+RJ12_2_GREEN = const(33)
+RJ12_2_YELLOW = const(14)
+RJ12_2_BLUE_INPUT_ONLY = const(34)
+
+
+AUX1_YELLOW = const(13)
+AUX1_WHITE = const(12)
+
+AUX2_YELLOW = const(26)
+AUX2_WHITE = const(25)
+
+ML10_PWM_1 = const(15)
+ML10_PWM_2 = const(4)
+ML10_PWM_3 = const(16)
+ML10_PWM_4 = const(17)
+ML10_PWM_5 = const(5)
+ML10_PWM_6 = const(18)
+ML10_PWM_7 = const(19)
+ML10_PWM_8 = const(21)
+
+# 2nd ML10 connector
+ML10_1 = const(12)
+ML10_2 = const(13)
+ML10_3 = const(14)
+ML10_4 = const(25)
+ML10_5 = const(26)
+ML10_6 = const(27)
+ML10_7 = const(33)
+ML10_8 = const(34)
+
+
+if board.CANID is not None:
+    import can
+    can.init(board.CANID, rx=35, tx=32)
+    board.CAN = can
+    can.cancommon.send_poweron()
