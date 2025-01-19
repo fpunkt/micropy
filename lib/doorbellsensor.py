47e60ba418518c3b077108ed597fac686c9888d6
fpunkt <fpunkt@icloud.com>
2025-01-19 16:06:25 +0100

    Limit number of events sent

--- lib/doorbellsensor.py
+++ lib/doorbellsensor.py
@@ -11,15 +11,24 @@ import irqio
 import can
 import canid
 import canerror
+import time
 
 class DoorbellSensor(irqio.IRQIO):
     def __init__(self, portid, pinid, pullup=True, inverted=True):
         super().__init__(portid, pinid, pullup=pullup, inverted=inverted)
         self.msg = can.makemessage(canid.SENSOR_DOORBELL_PUSHED, 5, portid=portid)
-        #self.msg = can.makemessage(canid.SENSOR_MOTION, 5, portid=portid)
+        self.last_run_at = 0
+        self.set_changed_status(0)
 
-    # run is handled by the parent class irqio.IRQIO
-    # async def run(self):
-    #     self.set_changed_status(1)
-    #     self.send_message()
-    #     return True
+
+    async def run(self):
+        # be nice, don't flood messages
+        t = time.time()
+        print('DoorbellSensor.run() lrb = {}, diff={}'.format(self.last_run_before_ms, t - self.last_run_at))
+        if t - self.last_run_at <= 2:
+            return False
+        self.last_run_at = t
+        self.update_payload()
+        self.send_message()
+        return True
+
