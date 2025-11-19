ba4972a7c08ae09e630b285917fc3bb864010c4d
fpunkt <fpunkt@icloud.com>
2025-11-19 23:44:45 +0100

    Added stubs to silient pylance warnings

--- mupystubs/uasyncio.py
+++ mupystubs/uasyncio.py
@@ -1,10 +1,10 @@
 """stubs for uasyncio module"""
 
-def sleep_ms(ms):
+async def sleep_ms(ms):
     """Sleep for the given number of milliseconds."""
     pass
 
-def sleep(secs):
+async def sleep(secs):
     """Sleep for the given number of seconds."""
     pass
 
