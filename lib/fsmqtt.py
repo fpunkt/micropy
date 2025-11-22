68bba9b40a8c3d3d5a3e84048bc6517f4c0b47aa
fpunkt <fpunkt@icloud.com>
2025-11-23 00:24:51 +0100

    Add default topic prefix, call callbacks with string arguments

--- lib/fsmqtt.py
+++ lib/fsmqtt.py
@@ -4,6 +4,9 @@ MQTT support. Use in main like
 from umqttsimple import MQTTClient
 import fsmqtt
 
+fsmqtt.connect('kegellampe')
+
+
 fsmqtt.connect(MQTTClient(clientid, fsmqtt.secrets.mqtt_server), name="myname", reset_on_error=True)
 
 fsmqtt.connect(MQTTClient(board.LOCATION, fsmqtt.secrets.mqtt_server))
@@ -29,22 +32,42 @@ class _Mqttoptions:
     def __init__(self) -> None:
         self.reset_on_error = False
         self.name = "undefined"
+        self.topic = "fsmqtt/undefined/"
 
 options = _Mqttoptions()
 _callbacks = dict()
 
+def _safe_decode(value):
+    """Convert bytes to utf-8 string if possible, otherwise return repr(value)."""
+    if isinstance(value, bytes):
+        try:
+            return value.decode('utf-8')
+        except Exception:
+            return repr(value)
+    return value
+
 def connect(client, name=None, reset_on_error=True):
+
+    if isinstance(client, str):
+        import umqttsimple
+        if name is None:
+            name = client
+        client = umqttsimple.MQTTClient(client, '192.168.178.5')
+
     options.name = name if name is not None else board.LOCATION
     options.reset_on_error = reset_on_error
 
+    if 'undefined' in options.topic:
+        options.topic = 'fsmqtt/' + options.name + '/'
+
     while True:
         try:
-            net.start_wlan()
+            net.start_wlan(32)
             board.MQTT = client
             board.MQTT.connect()
             if board.DEBUG:
                 print('MQTT connected')
-            board.MQTT.publish("info", options.name+"/connected")
+            publish("info", "connected")
             return True
         except OSError as e:
             if board.DEBUG:
@@ -56,7 +79,8 @@ def connect(client, name=None, reset_on_error=True):
             machine.reset()
 
 
-def publish(topic, message):
+def publish_raw(topic, message):
+    """publish message to topic without prefixing with options.topic"""
     if board.MQTT:
         try:
             board.MQTT.publish(topic, message)
@@ -68,13 +92,21 @@ def publish(topic, message):
                 machine.reset()
     return False
 
+def publish(topic, message):
+    """publish message to topic, prefixing with options.topic"""
+    return publish_raw(options.topic + topic, message)
+
 def _mqtt_callback(topic, message):
     # print("got MQTT message: T='{}, M='{}'".format(topic, message))
+    # try to decode topic/message to strings for easier handling by callbacks
+    m_str = _safe_decode(message)
+    # prefer exact bytes-key match, fall back to decoded-string key
     cb = _callbacks.get(topic, None)
     if cb is not None:
         if False:
-            print('# MQTT runnning callback for T={}, M={}'.format(topic, message))
-        cb(topic, message)
+            print('# MQTT running callback for T={}, M={}'.format(t_str, m_str))
+        # call callback with decoded strings
+        cb(message, m_str)
 
 async def _mqtt_poller_task():
     while True:
@@ -89,13 +121,24 @@ async def _mqtt_poller_task():
 board.BACKGROUND_RUNNERS.append(_mqtt_poller_task())
 
 
-def subscribe(topic, callback):
+def subscribe_raw(topic, callback):
+    """subscribe to topic without prefixing with options.topic"""
     if not board.MQTT:
         return
     if not isinstance(topic, bytes):
         topic = bytes(topic, 'utf-8')
     _callbacks[topic] = callback
+#    # also store the decoded string form for convenience
+#    try:
+#        _callbacks[_safe_decode(topic)] = callback
+#    except Exception:
+#        pass
     if board.DEBUG:
         print('MQTT subscribed to {}'.format(topic))
     board.MQTT.set_callback(_mqtt_callback)
     board.MQTT.subscribe(topic)
+
+
+def subscribe(topic, callback):
+    """subscribe to topic, prefixing with options.topic"""
+    subscribe_raw(options.topic + topic, callback)
